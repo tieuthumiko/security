dee689231292bb7194aa12293b28bd93066e256c
tieuthumiko <nguyenquangnhatminh0101121791@gmail.com>
2026-02-24 14:28:06 +0700

    fix smth

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Bot is running.');
+});
+
+app.listen(PORT, () => {
+  console.log(`Web server running on port ${PORT}`);
+});
+
 const {
   Client,
   GatewayIntentBits,
